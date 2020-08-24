bddaf557279f372e75dc035abebbf2174e71c813
acooke100 <acooke100@gmail.com>
2020-08-24 14:35:15 -0400

    Initial commit

---
+++ firstapp.js
@@ -0,0 +1,34 @@
+var Twitter = require('twitter');
+
+
+var client = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
+ 
+var joke;
+  
+var request = require('request');
+request('https://icanhazdadjoke.com', {json: true}, function (error, response, body)
+{
+	console.log('error:', error);
+	console.log('statusCode: ', response && response.statusCode);
+	console.log('body:', body);
+	joke = body.joke;
+	
+	
+	client.post('statuses/update', {status: joke},  function(error, tweet, response) {
+	
+	if(error) throw error;
+	console.log(tweet);  // Tweet body. 
+	console.log(response);  // Raw response object.
+	});
+});
+
+
+
+
+
+
